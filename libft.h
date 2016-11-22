fabee0ea8bea2a4228d728e10f608c3327803216
acolombe <acolombe@e2r4p7.42.fr>
2016-11-22 17:36:50 +0100

    no commit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: acolombe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/08 17:10:49 by acolombe          #+#    #+#             */
-/*   Updated: 2016/11/12 15:18:04 by acolombe         ###   ########.fr       */
+/*   Updated: 2016/11/22 14:09:31 by acolombe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-int		ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
