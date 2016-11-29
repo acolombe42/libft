637b9a9aadc51f1ab233cfa0e0ab90c71baf1b61
acolombe <acolombe@e2r7p5.42.fr>
2016-11-29 20:44:34 +0100

    a revoir

---
+++ ft_strlcat.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acolombe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/29 20:30:27 by acolombe          #+#    #+#             */
+/*   Updated: 2016/11/29 20:43:32 by acolombe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat (char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+
+	i = 0;
+	while (dst[i] && i < size)
+		i++;
+	j = i;
+	while (src[i - j] && i < size - 1)
+	{
+		dst[i] = src[i - j];
+		i++;
+	}
+	if (j < size)
+		dst[i] = '\0';
+	k = ft_strlen(src);
+	return (j + k);
+}
